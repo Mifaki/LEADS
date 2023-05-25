46bd8b6f6b130b3b6cf414e510a6ab938f4ca019
Faiz <agustianto.d19@gmail.com>
2023-05-25 21:22:38 +0700

    fix: fix read more bug

--- JS/profile.js
+++ JS/profile.js
@@ -1,30 +1,38 @@
+let resizeTimer;
+
 function toggleExperienceSubtitle() {
-  const experienceSubtitles = document.querySelectorAll('.experience-subtitle');
-  experienceSubtitles.forEach(subtitle => {
+  clearTimeout(resizeTimer);
+  const experienceSubtitles = document.querySelectorAll(".experience-subtitle");
+  experienceSubtitles.forEach((subtitle) => {
     const subtitleText = subtitle.textContent;
     if (subtitleText.length > 90) {
       const truncatedText = subtitleText.substring(0, 90);
       subtitle.innerHTML = `${truncatedText}<span class="ellipsis">...</span>`;
       subtitle.dataset.fullText = subtitleText;
-      subtitle.insertAdjacentHTML('afterend', '<button class="read-more-button">Read More</button>');
+      subtitle.insertAdjacentHTML(
+        "afterend",
+        '<button class="read-more-button">Read More</button>'
+      );
     }
   });
 
-  const readMoreButtons = document.querySelectorAll('.read-more-button');
-  readMoreButtons.forEach(button => {
-    button.addEventListener('click', () => {
+  const readMoreButtons = document.querySelectorAll(".read-more-button");
+  readMoreButtons.forEach((button) => {
+    button.addEventListener("click", () => {
       const subtitle = button.previousElementSibling;
       const fullText = subtitle.dataset.fullText;
-      if (subtitle.classList.contains('expanded')) {
+      if (subtitle.classList.contains("expanded")) {
         subtitle.innerHTML = `${fullText}<span class="ellipsis">...</span>`;
-        button.textContent = 'Read More';
+        button.textContent = "Read More";
       } else {
         subtitle.innerHTML = fullText;
-        button.textContent = 'Read Less';
+        button.textContent = "Read Less";
       }
-      subtitle.classList.toggle('expanded');
+      subtitle.classList.toggle("expanded");
     });
-  });
+  }, 200);
 }
 
-toggleExperienceSubtitle();
+if (window.innerWidth <= 600) {
+  toggleExperienceSubtitle();
+}
