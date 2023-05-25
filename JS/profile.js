284be482580f19db57bec3e63341bcdbb50ee10d
Faiz <agustianto.d19@gmail.com>
2023-05-25 20:45:42 +0700

    feat: add responsive profile page

---
+++ JS/profile.js
@@ -0,0 +1,30 @@
+function toggleExperienceSubtitle() {
+  const experienceSubtitles = document.querySelectorAll('.experience-subtitle');
+  experienceSubtitles.forEach(subtitle => {
+    const subtitleText = subtitle.textContent;
+    if (subtitleText.length > 90) {
+      const truncatedText = subtitleText.substring(0, 90);
+      subtitle.innerHTML = `${truncatedText}<span class="ellipsis">...</span>`;
+      subtitle.dataset.fullText = subtitleText;
+      subtitle.insertAdjacentHTML('afterend', '<button class="read-more-button">Read More</button>');
+    }
+  });
+
+  const readMoreButtons = document.querySelectorAll('.read-more-button');
+  readMoreButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      const subtitle = button.previousElementSibling;
+      const fullText = subtitle.dataset.fullText;
+      if (subtitle.classList.contains('expanded')) {
+        subtitle.innerHTML = `${fullText}<span class="ellipsis">...</span>`;
+        button.textContent = 'Read More';
+      } else {
+        subtitle.innerHTML = fullText;
+        button.textContent = 'Read Less';
+      }
+      subtitle.classList.toggle('expanded');
+    });
+  });
+}
+
+toggleExperienceSubtitle();
