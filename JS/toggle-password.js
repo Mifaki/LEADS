b12b9bc9293d46032ff0b3d994d7df8d195a8d95
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-05-25 18:55:39 +0700

    refactor: Code indentation

--- JS/toggle-password.js
+++ JS/toggle-password.js
@@ -2,8 +2,8 @@ const togglePassword = document.getElementById("toggle-password");
 const password = document.getElementById("password");
 
 togglePassword.addEventListener("click", () => {
-    password.type = password.type === "password" ? "text" : "password";
-    togglePassword.src = togglePassword.src.includes("eye-solid.svg")
-        ? "../../../Assets/eye-slash-solid.svg"
-        : "../../../Assets/eye-solid.svg";
+  password.type = password.type === "password" ? "text" : "password";
+  togglePassword.src = togglePassword.src.includes("eye-solid.svg")
+    ? "../../../Assets/eye-slash-solid.svg"
+    : "../../../Assets/eye-solid.svg";
 });
