b042ff8242befced209362ee9247e29434e913fc
Faiz <agustianto.d19@gmail.com>
2023-06-08 19:53:21 +0700

    feat: add logic to landing page button

---
+++ JS/landing.js
@@ -0,0 +1,8 @@
+function checkPassword() {
+  var password = localStorage.getItem("password");
+  if (password !== null && password !== undefined) {
+    window.location.href = "home.html";
+  } else {
+    window.location.href = "index.html";
+  }
+}
