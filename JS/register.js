ab864c6e0dba82c920a3f7071df5b8a9dac67f90
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-06-08 20:40:19 +0700

    `refactor:` Fix detail username and Changed landing image

--- JS/register.js
+++ JS/register.js
@@ -1,26 +1,24 @@
-document
-  .getElementById("registration-form")
-  .addEventListener("submit", function (event) {
-    
-    const username = document.getElementById("username").value.trim();
-    const email = document.getElementById("email").value.trim();
-    const password = document.getElementById("password").value.trim();
-    const passwordConfirmation = document.getElementById("passwordConfirmation");
+document.getElementById("registration-form").addEventListener("submit", function (event) {
+  event.preventDefault();
 
-    if (password !== passwordConfirmation.value) {
-      passwordConfirmation.setCustomValidity(
-        "Password and confirmation do not match"
-      );
-      event.preventDefault();
-      return;
-    }
+  const username = document.getElementById("username").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const password = document.getElementById("password").value.trim();
+  const passwordConfirmation = document.getElementById("passwordConfirmation");
 
-    passwordConfirmation.setCustomValidity("");
+  console.log("Im here");
 
-    localStorage.setItem("username", username);
-    localStorage.setItem("email", email);
-    localStorage.setItem("password", password);
+  if (password !== passwordConfirmation.value) {
+    alert("Password and confirmation do not match");
+    return;
+  }
 
-    event.preventDefault();
-    window.location.href = "index.html";
-  });
+  passwordConfirmation.setCustomValidity("");
+
+  localStorage.setItem("username", username);
+  localStorage.setItem("email", email);
+  localStorage.setItem("password", password);
+
+  event.preventDefault();
+  window.location.href = "index.html";
+});
