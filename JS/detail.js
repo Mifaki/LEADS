25dc8e45e06e9c14476f7aba40c0eedb51e895fd
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2023-06-09 08:01:40 +0700

    `fix:` Fix details page username

--- JS/detail.js
+++ JS/detail.js
@@ -1,3 +1,5 @@
+document.getElementById("user-name").innerHTML = localStorage.getItem("username").split(" ")[0] ?? "Budi";
+
 const regisButton = document.querySelector(".regis-button");
 
 regisButton.addEventListener("click", function() {
